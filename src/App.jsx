2ddaa36139874e3924fa984614ab8863168f5f28
brandonwirz <bwirz75@gmail.com>
2025-05-12 15:58:11 -0600

    pass down props/State at top

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,24 @@ import Pad from "./Pad"
 export default function App({ darkMode }) {
   const [pads, setPads] = React.useState(padsData)
 
+  function toggle(id) {
+    console.log("Clicked!")
+    setPads((prevPads) =>
+      prevPads.map((item) => {
+        return item.id === id ? { ...item, on: !item.on } : item
+      })
+    )
+    // map over pads array if current item has same id as the one passed to the function then flip it's on value
+  }
+
   const buttonElements = pads.map((pad) => (
-    <Pad key={pad.id} color={pad.color} on={pad.on} />
+    <Pad
+      toggle={toggle}
+      id={pad.id}
+      key={pad.id}
+      color={pad.color}
+      on={pad.on}
+    />
   ))
 
   return (
--- src/Pad.jsx
+++ src/Pad.jsx
@@ -1,15 +1,12 @@
 import React from "react"
 export default function (props) {
-  const [on, setOn] = React.useState(props.on)
+  console.log(props.id)
 
-  function toggle() {
-    setOn((prevOn) => !prevOn)
-  }
   return (
     <button
       style={{ backgroundColor: props.color }}
-      className={on ? "on" : "null"}
-      onClick={toggle}
+      className={props.on ? "on" : "null"}
+      onClick={() => props.toggle(props.id)}
     ></button>
   )
 }
