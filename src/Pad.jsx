c676f858aac59d89d4a0dc4788b32cb448dd22f1
brandonwirz <bwirz75@gmail.com>
2025-05-12 15:21:12 -0600

    sound pads commit 1

---
+++ src/Pad.jsx
@@ -0,0 +1,9 @@
+export default function (props) {
+  console.log(props.on)
+  return (
+    <button
+      style={{ backgroundColor: props.color }}
+      className={props.on ? "on" : "null"}
+    ></button>
+  )
+}
